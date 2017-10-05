3af281659965ff9ad3dfe41a04d70bd3d0cefdc7
RGBboy <l-_-l@rgbboy.com>
2017-10-05 17:42:57 -0400

    refactor Main;

--- src/Main.elm
+++ src/Main.elm
@@ -1,26 +1,26 @@
 port module Main exposing (..)
 
-import Color exposing (..)
+import Color exposing (Color)
 
 
--- import Signal
--- import Signal.Extra
-import Time
--- import Touch exposing (Touch)
 
-import Html exposing (..)
+
+import Html as H exposing (Html)
 import Html.Attributes as A
 import Html.Events as Events
-
 import Mouse
+import Time
 import Window
 
 
-port output : Output -> Cmd msg
+
+
+
+
 
 main : Program Never Model Msg
 main =
-  Html.program
+  H.program
     { init = init
     , view = view
     , update = update
@@ -28,6 +28,24 @@ main =
     }
 
 
+
+-- PORTS
+
+port output : Output -> Cmd msg
+
+type alias Output = (Bool, Float, Float)
+
+toOutput : Model -> Output
+toOutput { active, position, dimensions } =
+  let
+    (x, y) = normalize position dimensions
+    isActive = (active && x < 1 && x > 0 && y < 1 && y > 0)
+  in
+    (isActive, x, y)
+
+
+
+-- MODEL
 
 type alias Model =
   { active: Bool
@@ -46,18 +64,7 @@ init =
 
 
 
-
-type alias Output = (Bool, Float, Float)
-
-toOutput : Model -> Output
-toOutput { active, position, dimensions } =
-  let
-    (x, y) = normalize position dimensions
-    isActive = (active && x < 1 && x > 0 && y < 1 && y > 0)
-  in
-    (isActive, x, y)
-
-
+-- UPDATE
 
 type Msg
   = Active Bool
@@ -102,48 +109,7 @@ update message { active, position, dimensions } =
 
 
 
-view : Model -> Html Msg
-view { dimensions } =
-  let
-    (width, height) = dimensions
-    (w, h) = (toString width, toString height)
-    (x, y) = (toString (toFloat (width - 320) / 2), toString (toFloat (height - 320) / 2))
-  in
-    div
-      [ A.style
-          [ ("backgroundColor", "#333333")
-          , ("width", w ++ "px")
-          , ("height", h ++ "px")
-          ]
-      ]
-      [ div
-          [ A.style
-              [ ("backgroundColor", "#000000")
-              , ("position", "absolute")
-              , ("left", x ++ "px")
-              , ("top", y ++ "px")
-              , ("width", "320px")
-              , ("height", "320px")
-              ]
-          ]
-          []
-      ]
-
--- touchToActive : (List Touch) -> Action
--- touchToActive touches =
---   case touches of
---     hd :: tl -> Active True
---     [] -> Active False
---
--- touchToPosition : (List Touch) -> Action
--- touchToPosition touches =
---   case touches of
---     hd :: tl -> Position (hd.x, hd.y)
---     [] -> Position (0, 0)
-
-
-
-
+-- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -161,28 +127,33 @@ subscriptions model =
       , moves
       ]
 
--- mouseActiveSignal : Signal Action
--- mouseActiveSignal =
---   Signal.map Active Mouse.isDown
 
--- mousePositionSignal : Signal Action
--- mousePositionSignal =
---   Signal.map Position Mouse.position
 
--- touchActiveSignal : Signal Action
--- touchActiveSignal =
---   Signal.map touchToActive Touch.touches
---
--- touchPositionSignal : Signal Action
--- touchPositionSignal =
---   Signal.map touchToPosition (Time.delay 0 Touch.touches)
+-- VIEW
 
--- actionSignal : Signal Action
--- actionSignal =
---   Signal.mergeMany
---     [ resizeSignal
---     , mouseActiveSignal
---     , mousePositionSignal
---     , touchPositionSignal
---     , touchActiveSignal
---     ]
+view : Model -> Html Msg
+view { dimensions } =
+  let
+    (width, height) = dimensions
+    (w, h) = (toString width, toString height)
+    (x, y) = (toString (toFloat (width - 320) / 2), toString (toFloat (height - 320) / 2))
+  in
+    H.div
+      [ A.style
+          [ ("backgroundColor", "#333333")
+          , ("width", w ++ "px")
+          , ("height", h ++ "px")
+          ]
+      ]
+      [ H.div
+          [ A.style
+              [ ("backgroundColor", "#000000")
+              , ("position", "absolute")
+              , ("left", x ++ "px")
+              , ("top", y ++ "px")
+              , ("width", "320px")
+              , ("height", "320px")
+              ]
+          ]
+          []
+      ]
