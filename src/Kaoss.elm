9f04e17551c150a4a95340f037f17136d0f7a87a
RGBboy <l-_-l@rgbboy.com>
2017-10-09 19:16:19 -0400

    refactor kaoss model to use maybe for position;

--- src/Kaoss.elm
+++ src/Kaoss.elm
@@ -11,27 +11,22 @@ import AudioGraph exposing (AudioGraph)
 import Color exposing (Color)
 import Html as H exposing (Html)
 import Html.Attributes as A
-import Json.Encode as Encode
 import MouseEvents as Mouse
 import SingleTouch
-import Time
 import Touch
-import Window
 
 
 
 -- MODEL
 
 type alias Model =
-  { isActive : Bool
-  , position : (Float, Float)
+  { position : Maybe (Float, Float)
   , dimensions : (Float, Float)
   }
 
 init : (Float, Float) -> Model
 init dimensions =
-  { isActive = False
-  , position = (0, 0)
+  { position = Nothing
   , dimensions = dimensions
   }
 
@@ -39,40 +34,32 @@ init dimensions =
 
 -- UPDATE
 
--- Can remodel this to remove active and have Maybe (Float, Float) for position
 type Msg
-  = Active Bool
+  = Start (Float, Float)
   | Position (Float, Float)
+  | Stop
 
 update : Msg -> Model -> Model
-update message ({ isActive, position, dimensions } as model) =
+update message ({ position, dimensions } as model) =
   case message of
-    Active isActive ->
-      { model | isActive = isActive }
-    Position position ->
-      { model | position = position }
+    Start value ->
+      { model | position = Just value }
+    Position value ->
+      { model | position = Maybe.map (always value) position }
+    Stop ->
+      { model | position = Nothing }
 
 
 
 -- GRAPH
 
 normalize : (Float, Float) -> (Float, Float) -> (Float, Float)
-normalize (aX, aY) (bX, bY) =
+normalize (bX, bY) (aX, aY) =
   (aX / bX, aY / bY)
-
-scale : number -> (number, number) -> (number, number)
-scale factor (x, y) =
-  (x * factor, y * factor)
 
 clamp : (number, number) -> (number, number) -> (number, number) -> (number, number)
 clamp (minX, minY) (maxX, maxY) (x, y) =
   (Basics.clamp minX maxX x, Basics.clamp minY maxY y)
-
-boolToFloat : Bool -> number
-boolToFloat value =
-  case value of
-    True -> 1
-    False -> 0
 
 frequencyRatio : Float -> Float
 frequencyRatio value =
@@ -97,10 +84,10 @@ createGraph id output g f =
     ]
 
 graph : String -> AudioGraph.Destination -> Model -> AudioGraph
-graph id output { isActive, dimensions, position } =
-  normalize position dimensions
+graph id output { dimensions, position } =
+  Maybe.map (normalize dimensions) position
+    |> Maybe.withDefault (0, 0)
     |> clamp (0, 0) (1, 1)
-    |> scale (boolToFloat isActive)
     |> (uncurry (createGraph id output))
 
 
@@ -116,14 +103,12 @@ intToFloat (x, y) =
   (toFloat x, toFloat y)
 
 view : Model -> Html Msg
-view { isActive, dimensions } =
+view { dimensions, position } =
   let
     (width, height) = dimensions
     (w, h) = (toString width, toString height)
-    color =
-      case isActive of
-        True -> "#333333"
-        False -> "#000000"
+    color = Maybe.map (always "#333333") position
+      |> Maybe.withDefault "#000000"
   in
     H.div
       [ A.style
@@ -139,13 +124,13 @@ view { isActive, dimensions } =
               , ("width", "100%")
               , ("height", "100%")
               ]
-          , Mouse.onMouseDown (always <| Active True)
-          , Mouse.onMouseUp (always <| Active False)
-          , Mouse.onMouseOut (always <| Active False)
+          , Mouse.onMouseDown (Mouse.relPos >> tupleFromXY >> intToFloat >> Start)
+          , Mouse.onMouseUp (always Stop)
+          , Mouse.onMouseOut (always Stop)
           , Mouse.onMouseMove (Mouse.relPos >> tupleFromXY >> intToFloat >> Position)
-          , SingleTouch.onStart (always (Active True))
-          , SingleTouch.onEnd (always (Active False))
-          , SingleTouch.onCancel (always (Active False))
+          , SingleTouch.onStart (Touch.clientPos >> Start)
+          , SingleTouch.onEnd (always Stop)
+          , SingleTouch.onCancel (always Stop)
           , SingleTouch.onMove (Touch.clientPos >> Position)
           ]
           []
