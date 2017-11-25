5116ded190da8331eedb6dd66a8a62c1f4b212a0
RGBboy <l-_-l@rgbboy.com>
2017-11-25 19:17:53 +0000

    add new audio properties; Allow stacks of timing for audio properties;

--- src/AudioGraph.elm
+++ src/AudioGraph.elm
@@ -7,10 +7,11 @@ module AudioGraph exposing
   , connectTo, output
   , AudioProperty
   , sineWave, squareWave, sawtoothWave
-  , frequency, gain, detune
+  , frequency, gain, gainLinearRampToValueAtTime, gainValueAtTime, gainExponentialRampToValueAtTime, detune
   , encode
   )
 
+import Dict exposing (Dict)
 import Json.Encode as Encode
 
 
@@ -32,10 +33,16 @@ type Destination
   = Output
   | Connect String
 
+type Value
+  = Scalar Float
+  | ValueAtTime Float Float
+  | LinearRampToValueAtTime Float Float
+  | ExponentialRampToValueAtTime Float Float
+
 type AudioProperty
   = WaveType Wave
   | Frequency Float
-  | Gain Float
+  | Gain Value
   | Detune Int
 
 type Wave
@@ -74,7 +81,16 @@ frequency : Float -> AudioProperty
 frequency = Frequency
 
 gain : Float -> AudioProperty
-gain = Gain
+gain value = Gain (Scalar value)
+
+gainValueAtTime : Float -> Float -> AudioProperty
+gainValueAtTime value time = Gain (ValueAtTime value time)
+
+gainLinearRampToValueAtTime : Float -> Float -> AudioProperty
+gainLinearRampToValueAtTime value time = Gain (LinearRampToValueAtTime value time)
+
+gainExponentialRampToValueAtTime : Float -> Float -> AudioProperty
+gainExponentialRampToValueAtTime value time = Gain (ExponentialRampToValueAtTime value time)
 
 detune : Int -> AudioProperty
 detune = Detune
@@ -95,14 +111,6 @@ encodeDestination destination =
     Output -> Encode.string "output"
     Connect id -> Encode.string id
 
-encodeAudioProperty : AudioProperty -> (String, Encode.Value)
-encodeAudioProperty property =
-  case property of
-    WaveType value -> ("type", encodeWave value)
-    Frequency value -> ("frequency", Encode.float value)
-    Gain value -> ("gain", Encode.float value)
-    Detune value -> ("detune", Encode.int value)
-
 encodeWave : Wave -> Encode.Value
 encodeWave wave =
   case wave of
@@ -110,13 +118,58 @@ encodeWave wave =
     Square -> Encode.string "square"
     Sawtooth -> Encode.string "sawtooth"
 
+encodeValue : Value -> Encode.Value
+encodeValue value =
+  case value of
+    Scalar v -> Encode.float v
+    ValueAtTime v t ->
+      Encode.list
+        [ Encode.string "setValueAtTime"
+        , Encode.float v
+        , Encode.float t
+        ]
+    LinearRampToValueAtTime v t ->
+      Encode.list
+        [ Encode.string "linearRampToValueAtTime"
+        , Encode.float v
+        , Encode.float t
+        ]
+    ExponentialRampToValueAtTime v t ->
+      Encode.list
+        [ Encode.string "exponentialRampToValueAtTime"
+        , Encode.float v
+        , Encode.float t
+        ]
+
+updateAudioProperty : Encode.Value -> Maybe (List Encode.Value) -> Maybe (List Encode.Value)
+updateAudioProperty value existing =
+  Maybe.map ((::) value) existing
+    |> Maybe.withDefault [value]
+    |> Just
+
+foldAudioProperties : AudioProperty -> Dict String (List Encode.Value) -> Dict String (List Encode.Value)
+foldAudioProperties property collection =
+  case property of
+    WaveType wave -> Dict.insert "type" [ encodeWave wave ] collection
+    Frequency value -> Dict.insert "frequency" [ Encode.float value ] collection
+    Gain value -> Dict.update "gain" (updateAudioProperty (encodeValue value)) collection
+    Detune value -> Dict.insert "detune" [ Encode.int value ] collection
+
+-- Temporary -- Will need to be updated to work correctly with properties that should just have a single value and properties that can have multiple
+encodeAudioPropertyValue : (String, List Encode.Value) -> (String, Encode.Value)
+encodeAudioPropertyValue (key, list) =
+  case list of
+    a::b::_ -> (key, Encode.list list)
+    a::_ -> (key, a)
+    [] -> (key, Encode.null)
+
 encodeNode : AudioNode -> (String, Encode.Value)
 encodeNode node =
   ( node.id
   , Encode.list
       [ encodeNodeType node.nodeType
       , encodeDestination node.destination
-      , List.map encodeAudioProperty node.properties |> Encode.object
+      , List.foldr foldAudioProperties Dict.empty node.properties |> Dict.toList |> List.map encodeAudioPropertyValue |> Encode.object
       ]
   )
 
