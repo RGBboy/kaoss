b4087ce62b7339741c51585fc46fe40e3ae519f3
RGBboy <l-_-l@rgbboy.com>
2017-12-02 18:23:13 +0000

    refactor AudioGraph API to be more succinct;

--- src/AudioGraph.elm
+++ src/AudioGraph.elm
@@ -2,12 +2,12 @@ module AudioGraph exposing
   ( AudioGraph
   , audioGraph
   , audioNode
-  , oscillator, gainNode
   , Destination
   , connectTo, output
-  , AudioProperty
+  , AudioParam
   , sineWave, squareWave, sawtoothWave
-  , frequency, gain, gainLinearRampToValueAtTime, gainValueAtTime, gainExponentialRampToValueAtTime, detune
+  , gain
+  , value, valueAtTime, linearRampToValueAtTime, exponentialRampToValueAtTime
   , encode
   )
 
@@ -19,48 +19,69 @@ import Json.Encode as Encode
 type alias AudioGraph = List AudioNode
 
 type alias AudioNode =
-  { nodeType : NodeType
-  , id : String
+  { id : String
   , destination : Destination
-  , properties : List AudioProperty
+  , node : Node
   }
 
-type NodeType
-  = OscillatorNode
-  | GainNode
+type Node
+  = OscillatorNode WaveType Frequency Detune
+  | GainNode Gain
+
+type WaveType
+  = Sine
+  | Square
+  | Sawtooth
+
+type Frequency =
+  Frequency (List AudioParam)
+
+type Detune =
+  Detune (List AudioParam)
+
+type Gain =
+  Gain (List AudioParam)
 
 type Destination
   = Output
   | Connect String
 
-type Value
+type AudioParam
   = Scalar Float
   | ValueAtTime Float Float
   | LinearRampToValueAtTime Float Float
   | ExponentialRampToValueAtTime Float Float
 
-type AudioProperty
-  = WaveType Wave
-  | Frequency Float
-  | Gain Value
-  | Detune Int
-
-type Wave
-  = Sine
-  | Square
-  | Sawtooth
-
 audioGraph : List AudioNode -> AudioGraph
 audioGraph = identity
 
-audioNode : NodeType -> String -> Destination -> List AudioProperty -> AudioNode
+audioNode : String -> Destination -> Node -> AudioNode
 audioNode = AudioNode
 
-oscillator : String -> Destination -> List AudioProperty -> AudioNode
-oscillator = AudioNode OscillatorNode
+sineWave : Float -> Float -> Node
+sineWave frequency detune =
+  OscillatorNode
+    Sine
+    (Frequency [Scalar frequency])
+    (Detune [Scalar detune])
 
-gainNode : String -> Destination -> List AudioProperty -> AudioNode
-gainNode = AudioNode GainNode
+squareWave : Float -> Float -> Node
+squareWave frequency detune =
+  OscillatorNode
+    Square
+    (Frequency [Scalar frequency])
+    (Detune [Scalar detune])
+
+sawtoothWave : Float -> Float -> Node
+sawtoothWave frequency detune =
+  OscillatorNode
+    Sawtooth
+    (Frequency [Scalar frequency])
+    (Detune [Scalar detune])
+
+gain : List AudioParam -> Node
+gain params =
+  GainNode (Gain params)
 
 connectTo : String -> Destination
 connectTo = Connect
@@ -68,42 +89,21 @@ connectTo = Connect
 output : Destination
 output = Output
 
-sineWave : AudioProperty
-sineWave = WaveType Sine
+value : Float -> AudioParam
+value v = Scalar v
 
-squareWave : AudioProperty
-squareWave = WaveType Square
+valueAtTime : Float -> Float -> AudioParam
+valueAtTime v time = ValueAtTime v time
 
-sawtoothWave : AudioProperty
-sawtoothWave = WaveType Sawtooth
+linearRampToValueAtTime : Float -> Float -> AudioParam
+linearRampToValueAtTime v time = LinearRampToValueAtTime v time
 
-frequency : Float -> AudioProperty
-frequency = Frequency
-
-gain : Float -> AudioProperty
-gain value = Gain (Scalar value)
-
-gainValueAtTime : Float -> Float -> AudioProperty
-gainValueAtTime value time = Gain (ValueAtTime value time)
-
-gainLinearRampToValueAtTime : Float -> Float -> AudioProperty
-gainLinearRampToValueAtTime value time = Gain (LinearRampToValueAtTime value time)
-
-gainExponentialRampToValueAtTime : Float -> Float -> AudioProperty
-gainExponentialRampToValueAtTime value time = Gain (ExponentialRampToValueAtTime value time)
-
-detune : Int -> AudioProperty
-detune = Detune
+exponentialRampToValueAtTime : Float -> Float -> AudioParam
+exponentialRampToValueAtTime v time = ExponentialRampToValueAtTime v time
 
 
 
 -- ENCODER
-
-encodeNodeType : NodeType -> Encode.Value
-encodeNodeType nodeType =
-  case nodeType of
-    OscillatorNode -> Encode.string "oscillator"
-    GainNode -> Encode.string "gain"
 
 encodeDestination : Destination -> Encode.Value
 encodeDestination destination =
@@ -111,15 +111,8 @@ encodeDestination destination =
     Output -> Encode.string "output"
     Connect id -> Encode.string id
 
-encodeWave : Wave -> Encode.Value
-encodeWave wave =
-  case wave of
-    Sine -> Encode.string "sine"
-    Square -> Encode.string "square"
-    Sawtooth -> Encode.string "sawtooth"
-
-encodeValue : Value -> Encode.Value
-encodeValue value =
+encodeAudioParam : AudioParam -> Encode.Value
+encodeAudioParam value =
   case value of
     Scalar v -> Encode.float v
     ValueAtTime v t ->
@@ -141,38 +134,56 @@ encodeValue value =
         , Encode.float t
         ]
 
-updateAudioProperty : Encode.Value -> Maybe (List Encode.Value) -> Maybe (List Encode.Value)
-updateAudioProperty value existing =
-  Maybe.map ((::) value) existing
-    |> Maybe.withDefault [value]
-    |> Just
-
-foldAudioProperties : AudioProperty -> Dict String (List Encode.Value) -> Dict String (List Encode.Value)
-foldAudioProperties property collection =
-  case property of
-    WaveType wave -> Dict.insert "type" [ encodeWave wave ] collection
-    Frequency value -> Dict.insert "frequency" [ Encode.float value ] collection
-    Gain value -> Dict.update "gain" (updateAudioProperty (encodeValue value)) collection
-    Detune value -> Dict.insert "detune" [ Encode.int value ] collection
-
 -- Temporary -- Will need to be updated to work correctly with properties that should just have a single value and properties that can have multiple
-encodeAudioPropertyValue : (String, List Encode.Value) -> (String, Encode.Value)
-encodeAudioPropertyValue (key, list) =
-  case list of
-    a::b::_ -> (key, Encode.list list)
-    a::_ -> (key, a)
-    [] -> (key, Encode.null)
+encodeAudioParams : List AudioParam -> Encode.Value
+encodeAudioParams params =
+  let
+    list = List.map encodeAudioParam params
+  in
+    case list of
+      a::b::_ -> Encode.list list
+      a::_ -> a
+      [] -> Encode.null
 
-encodeNode : AudioNode -> (String, Encode.Value)
+encodeWaveType : WaveType -> Encode.Value
+encodeWaveType waveType =
+  case waveType of
+    Sine -> Encode.string "sine"
+    Square -> Encode.string "square"
+    Sawtooth -> Encode.string "sawtooth"
+
+encodeNode : Node -> (String, Encode.Value)
 encodeNode node =
-  ( node.id
-  , Encode.list
-      [ encodeNodeType node.nodeType
-      , encodeDestination node.destination
-      , List.foldr foldAudioProperties Dict.empty node.properties |> Dict.toList |> List.map encodeAudioPropertyValue |> Encode.object
-      ]
-  )
+  case node of
+    OscillatorNode waveType (Frequency frequency) (Detune detune) ->
+      ( "oscillator"
+      , [ ( "type", encodeWaveType waveType )
+        , ( "frequency", encodeAudioParams frequency )
+        , ( "detune", encodeAudioParams detune )
+        ]
+        |> Encode.object
+      )
+    GainNode (Gain gain) ->
+      ( "gain"
+      , [ ("gain", encodeAudioParams gain)
+        ]
+        |> Encode.object
+      )
+
+encodeAudioNode : AudioNode -> (String, Encode.Value)
+encodeAudioNode audioNode =
+  let
+    (kind, audioParams) = encodeNode audioNode.node
+  in
+
+    ( audioNode.id
+    , Encode.list
+        [ Encode.string kind
+        , encodeDestination audioNode.destination
+        , audioParams
+        ]
+    )
 
 encode : AudioGraph -> Encode.Value
 encode graph =
-  List.map encodeNode graph |> Encode.object
+  List.map encodeAudioNode graph |> Encode.object
